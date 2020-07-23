f14b730a46533e7f0f22be06a219fb28ab91fb6e
lzw981731 <827520890@qq.com>
2020-07-23 23:09:28 +0800

    Update hostloc2tg.py

--- hostloc2tg.py
+++ hostloc2tg.py
@@ -71,8 +71,8 @@ def mark_key(key):
     post_url = os.environ["post_url"]
 def post(chat_id, text):
     try:
-     text = parse.quote(text)
-      post_url = '"post_url"' \
+        text = parse.quote(text)
+        post_url = '"post_url"' \
                    '?parse_mode=MarkdownV2&chat_id={0}&text={1}'.format(chat_id, text)
         headers = {
             'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36'}
